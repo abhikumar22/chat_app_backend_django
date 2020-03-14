e0707470bf78604a505abb8dc88bca50c906f73b
Abhishek kumar <abhi.ckp1002@gmail.com>
2020-03-14 13:47:31 +0530

    initial commit

---
+++ userchatapp/api.py
@@ -0,0 +1,122 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
+import json
+from uuid import uuid4
+from decimal import Decimal
+from userchatapp.models import Auth, Chat, User
+
+from rest_framework.pagination import PageNumberPagination
+from django.core.paginator import Paginator
+from profanity_check import predict, predict_prob
+
+
+
+
+
+class Login(APIView):
+    def post(self, request):
+        user_name = request.data.get("username")
+        # u = User(username=request.data.get("username"))
+        kk = User.objects.filter(username=user_name)
+        authToken = 0
+        uid = 0
+        if kk.count() > 0:  # OLD user
+            print("*****OLD USER******")
+            rand_token = uuid4()
+
+            auth = Auth()
+            auth.uid = kk[0]
+            auth.token = rand_token
+            auth.save()
+
+            authToken = rand_token
+            uid = kk[0].id
+            res = 1
+        else:  # New user
+            print("*****New USER******", kk)
+            u = User(username=request.data.get("username"))
+            u.save()
+
+            kk = User.objects.filter(username=request.data.get("username"))
+            # print("**",kk.values("username")[0].get("username"))
+
+            rand_token = uuid4()
+
+            auth = Auth()
+            auth.uid = kk[0]
+            auth.token = rand_token
+            auth.save()
+
+            authToken = rand_token
+            uid = kk[0].id
+
+            # print(kk.count())
+
+        data = {
+            "uid": uid,
+            "token": authToken
+        }
+        # u.save()
+        # serializer = UserSerializers(model,many=True)
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
+class GetAllUsers(APIView):
+    def get(self, request):
+        model = User.objects.all()
+        serializer = GetAllUserSerializer(model, many=True)
+        data = {
+            "data" : serializer.data,
+            "status":status.HTTP_200_OK
+        }
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+class GetAllChat(APIView):
+    def post(self, request):
+
+        senderId = request.data.get("yourUserId")
+        receiverId = request.data.get("otherUserId")
+        model1 = Chat.objects.filter(senderId=senderId, receiverId=receiverId)|Chat.objects.filter(senderId=receiverId, receiverId=senderId)
+        model = model1.order_by('created_at')
+        serializer = GetAllChatSerializer(model, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class AddChat(APIView):
+    def post(self, request):
+
+
+        senderId = request.data.get("senderId")
+        receiverId = request.data.get("receiverId")
+        message = request.data.get("message")
+        kp = predict_prob([message])
+        print("LLLLL",kp[0])
+        
+
+        send_user = User.objects.get(id=senderId)
+        recv_user = User.objects.get(id=receiverId)
+
+        chat_obj = Chat()
+        chat_obj.senderId = send_user
+        chat_obj.receiverId = recv_user
+        chat_obj.message = message
+        chat_obj.save()
+        sss = '0.500000000'
+        if(Decimal(kp[0]).compare(Decimal(sss))<=0):
+            stt = status.HTTP_200_OK
+        else:
+            stt = status.HTTP_308_PERMANENT_REDIRECT
+        
+
+        data = {
+            "message" : "added chat",
+            "status":stt
+        }
+
+
+        return Response(data, status=status.HTTP_200_OK)
---
+++ userchatapp/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    created_at = models.DateTimeField(default=timezone.now)
+
+class Auth(models.Model):
+    uid = models.ForeignKey(User,on_delete=models.DO_NOTHING, related_name='authtokens')
+    token=models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=timezone.now)
+
+class Chat(models.Model):
+    senderId = models.ForeignKey(User,on_delete=models.DO_NOTHING, related_name='sender_user_id')
+    receiverId = models.ForeignKey(User,on_delete=models.DO_NOTHING, related_name='receiver_user_id')
+    message = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
